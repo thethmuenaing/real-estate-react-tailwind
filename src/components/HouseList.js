306a1efb4f57ab05960e20039d240fb476ee2420
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-07-08 21:29:54 +0630

    add the loading text

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -23,7 +23,11 @@ const HouseList = () => {
 	}
 
 	if (houses.length < 1) {
-		return <div>Sorry, nothing found</div>;
+		return (
+			<div className="text-center text-3xl text-gray-400 mt-48">
+				Sorry, nothing found
+			</div>
+		);
 	}
 	return (
 		<section className="mb-20">
